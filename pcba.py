ae42db8bf6a4439f7af5edfe585d795f6317e28d
anfractuosity <anfractuosity@anfractuosity.com>
2020-05-23 22:46:18 +0100

    remove need for board file

--- pcba.py
+++ pcba.py
@@ -14,12 +14,19 @@ import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import pcbnew
 
-
-
 # Graph centroid file and generate output
-def graph(board,centroid,outfile):
+def graph(centroid,outfile,libraries):
 
-    board = pcbnew.LoadBoard(board)
+    # Colours for parts, hardcoded at the mo'
+    colours = {'R':"r",
+               'C':"b",
+               'L':"y",
+               'U':"black",
+               'Y':"purple",
+               'D':"pink",
+               'J':"grey",
+               'Q':"orange"}
+
     centroid_data = [] # represents centroid data
     boarddata = {}
 
@@ -50,18 +57,28 @@ def graph(board,centroid,outfile):
 
             centroid_data.append(line_data)
             line = centroid_input.readline()
-
-    ref = {'R':{"path":"/usr/share/kicad/modules/Resistor_SMD.pretty","colour":"r"},
-           'C':{"path":"/usr/share/kicad/modules/Capacitor_SMD.pretty","colour":"b"},
-           'L':{"path":"/usr/share/kicad/modules/Inductor_SMD.pretty/","colour":"y"}}
-
-    for mod in board.GetModules(): 
-        if mod.GetReference()[0] in ref:
-            fpid = mod.GetFPID()
-            footprint = pcbnew.FootprintLoad(ref[mod.GetReference()[0]]['path'], fpid.GetUniStringLibItemName())
-            m = pcbnew.MODULE(footprint)
-            boarddata[mod.GetReference()]  = (m.GetFootprintRect().GetWidth()/1e6,m.GetFootprintRect().GetHeight()/1e6)
     
+    # Obtain unique packages
+    packages = []
+    for part in centroid_data:
+        packages.append(part['Package'])
+    packages = set(packages)
+
+    # Obtain footprint for package
+    for package in packages:
+        for path in libraries:
+            found = False
+            for d in glob.glob(path+"/*"):
+                footprint = pcbnew.FootprintLoad(d,package)
+                if footprint == None:
+                    continue
+                m = pcbnew.MODULE(footprint)
+                boarddata[package]  = (m.GetFootprintRect().GetWidth()/1e6,m.GetFootprintRect().GetHeight()/1e6)
+                found = True
+                break
+            if found:
+                break
+
     # Generate centroid graph
     fig, ax = plt.subplots()
     color = 'orange'
@@ -75,12 +92,11 @@ def graph(board,centroid,outfile):
         yp = l['PosY']
         tr = matplotlib.transforms.Affine2D().rotate_deg_around(xp,yp,l['Rot'])
 
-        if l['Ref'][0] in ref :
-            a = boarddata[l['Ref']][0]
-            b = boarddata[l['Ref']][1]
-            col = ref[l['Ref'][0]]["colour"]
-            rect = patches.Rectangle((xp - (a/2),yp - (b/2)),a,b,linewidth=1,edgecolor='b',facecolor=col,transform=tr+ax.transData) #,angle=l['Rot'])
-            ax.add_patch(rect)
+        a = boarddata[l['Package']][0]
+        b = boarddata[l['Package']][1]
+        col = colours[l['Ref'][0]]
+        rect = patches.Rectangle((xp - (a/2),yp - (b/2)),a,b,linewidth=1,edgecolor='b',facecolor=col,transform=tr+ax.transData) #,angle=l['Rot'])
+        ax.add_patch(rect)
 
     ax.scatter([], [], c=color, label=color,
                alpha=0.3, edgecolors='none')
@@ -93,10 +109,10 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser(description="CLI for PCBA centroid parsing")
     parser.add_argument("--input",dest="input", type=str, help="KiCad centroid file",required=True)
-    parser.add_argument("--board",dest="board", type=str, help="KiCad board file",required=True)
+    parser.add_argument("--libraries",dest="libraries", type=str, help="KiCad directories that have footprints, comma separated",required=True)
     parser.add_argument("--output",dest="output",type=str, help="Image output",required=True)
     args = parser.parse_args()
 
     # generate graph from centroid file
-    graph(os.path.expanduser(args.board),os.path.expanduser(args.input),os.path.expanduser(args.output))
+    graph(os.path.expanduser(args.input),os.path.expanduser(args.output),args.libraries.split(","))
 
