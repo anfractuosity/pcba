7b7bbd660bc3d4fe98c3e07bef3800a217d858e2
anfractuosity <anfractuosity@anfractuosity.com>
2020-05-25 13:08:46 +0100

    tidy/simplify code

--- pcba.py
+++ pcba.py
@@ -19,54 +19,86 @@ import pcbnew
 
 # Get reference name, without number ID hopefully
 def getref(ref):
-    comp = re.match("(.*?)[0-9]+$",ref)
+    comp = re.match("(.*?)[0-9]+$", ref)
     if comp != None:
         return comp.group(1)
     else:
         return ref
 
-def image(refs,colours,centroid_data,boarddata,side,output):
+
+# Generate graph with components for particular side of the board
+def image(refs, colours, centroid_data, boarddata, side, output):
 
     # Generate centroid graph
     fig, ax = plt.subplots()
 
     for l in centroid_data:
 
-        if l['Side'] != side:
+        # Display components for this side of board
+        if l["Side"] != side:
             continue
 
-        if l['Package'] not in boarddata:
+        # If component package not found, ignore
+        if l["Package"] not in boarddata:
             continue
-       
-        xp = l['PosX']
-        yp = l['PosY']
-        tr = matplotlib.transforms.Affine2D().rotate_deg_around(xp,yp,l['Rot'])
-        a = boarddata[l['Package']]["size"][0]
-        b = boarddata[l['Package']]["size"][1]
+
+        xp = l["PosX"]
+        yp = l["PosY"]
+        tr = matplotlib.transforms.Affine2D().rotate_deg_around(xp, yp, l["Rot"])
+        a = boarddata[l["Package"]]["size"][0]
+        b = boarddata[l["Package"]]["size"][1]
         z = 0.5
-        comp = getref(l['Ref'])
+        comp = getref(l["Ref"])
         col = colours[comp]
-        rect = patches.Rectangle((xp - (a/2),yp - (b/2)),a,b,linewidth=1,edgecolor='b',facecolor=col) 
 
-        if "print" in boarddata[l['Package']]:
-            prin = boarddata[l['Package']]["print"] 
-            xp += prin[0]
-            yp -= prin[1]
-            rect = patches.Rectangle((xp - (a/2),yp - (b/2)),a,b,linewidth=1,edgecolor='b',facecolor=col) 
+        # Adjust x,y pos of component if centre isn't 0,0
+        if "centre" in boarddata[l["Package"]]:
+            centre = boarddata[l["Package"]]["centre"]
+            xp += centre[0]
+            yp -= centre[1]
 
-        if "pin" in boarddata[l['Package']]:
-            if l['Side'] == 'top':
-                pin1 = patches.Rectangle((xp + boarddata[l['Package']]["pin"][0] - (z/2),yp - boarddata[l['Package']]["pin"][1] - (z/2)),z,z,facecolor='white')
+        # Generate rectangle for component
+        rect = patches.Rectangle(
+            (xp - (a / 2), yp - (b / 2)),
+            a,
+            b,
+            linewidth=1,
+            edgecolor="b",
+            facecolor=col,
+        )
+
+        # If we found pin 1, display it depending if we're on the top/bottom of board
+        if "pin" in boarddata[l["Package"]]:
+            if l["Side"] == "top":
+                pin1 = patches.Rectangle(
+                    (
+                        xp + boarddata[l["Package"]]["pin"][0] - (z / 2),
+                        yp - boarddata[l["Package"]]["pin"][1] - (z / 2),
+                    ),
+                    z,
+                    z,
+                    facecolor="white",
+                )
             else:
-                pin1 = patches.Rectangle((xp - boarddata[l['Package']]["pin"][0] - (z/2),yp + boarddata[l['Package']]["pin"][1] - (z/2)),z,z,facecolor='white')
-            dat = [rect,pin1]
+                pin1 = patches.Rectangle(
+                    (
+                        xp - boarddata[l["Package"]]["pin"][0] - (z / 2),
+                        yp + boarddata[l["Package"]]["pin"][1] - (z / 2),
+                    ),
+                    z,
+                    z,
+                    facecolor="white",
+                )
+            dat = [rect, pin1]
         else:
             dat = [rect]
 
         collection = matplotlib.collections.PatchCollection(dat, match_original=True)
-        collection.set_transform(tr+ax.transData)
+        collection.set_transform(tr + ax.transData)
         ax.add_collection(collection)
-        ax.text(xp,yp,l['Ref'],fontsize=7,fontweight='bold',transform=tr+ax.transData)
+        ax.text(
+            xp, yp, l["Ref"], fontsize=7, fontweight="bold", transform=tr + ax.transData
+        )
 
     ax.scatter([], [])
 
@@ -81,9 +113,11 @@ def image(refs,colours,centroid_data,boarddata,side,output):
     plt.show()
     fig.savefig(output)
 
-def load(path,package):
 
-    footprint = pcbnew.FootprintLoad(path,package)
+# Attempt to load footprint for package
+def load(path, package):
+
+    footprint = pcbnew.FootprintLoad(path, package)
     data = {}
 
     if footprint != None:
@@ -97,40 +131,51 @@ def load(path,package):
                 pinv = p.GetPosition()
                 break
 
-        prin = (m.GetFootprintRect().GetCenter()[0]/1e6,m.GetFootprintRect().GetCenter()[1]/1e6)
-        data = {"print":prin,"size": (m.GetFootprintRect().GetWidth()/1e6,m.GetFootprintRect().GetHeight()/1e6)}
+        centre = (
+            m.GetFootprintRect().GetCenter()[0] / 1e6,
+            m.GetFootprintRect().GetCenter()[1] / 1e6,
+        )
+        data = {
+            "centre": centre,
+            "size": (
+                m.GetFootprintRect().GetWidth() / 1e6,
+                m.GetFootprintRect().GetHeight() / 1e6,
+            ),
+        }
         if pinv is not None:
-            data["pin"] = (pinv[0]/1e6,pinv[1]/1e6)
-
+            data["pin"] = (pinv[0] / 1e6, pinv[1] / 1e6)
 
     return data
 
-class GetOutOfLoop( Exception ):
+
+# Used to escape from loops
+class GetOutOfLoop(Exception):
     pass
 
-# Graph centroid file and generate output
-def graph(centroid,libraries,top,bottom=''):
 
-    centroid_data = [] # represents centroid data
+# Graph centroid file and generate output
+def graph(centroid, libraries, top, bottom=""):
+
+    centroid_data = []  # represents centroid data
     boarddata = {}
 
     with open(centroid) as centroid_input:
-    
-        line = ""   # line contents
-        header = "" # file header
-        while (line := centroid_input.readline())[0] == '#':
+
+        line = ""  # line contents
+        header = ""  # file header
+        while (line := centroid_input.readline())[0] == "#":
             header = line.split()[1:]
             continue
 
         while line:
 
-            if line[0] == ' ':
+            if line[0] == " ":
                 line = centroid_input.readline()
                 continue
-            
-            lsplit = line.strip().split() # array of columns
 
-            if lsplit[0][0] == '#':
+            lsplit = line.strip().split()  # array of columns
+
+            if lsplit[0][0] == "#":
                 # at end of file
                 break
 
@@ -138,27 +183,27 @@ def graph(centroid,libraries,top,bottom=''):
             n = 0
             for col in header:
                 try:
-                    line_data[col] = float(lsplit[n]) # if data is number
+                    line_data[col] = float(lsplit[n])  # if data is number
                 except:
-                    line_data[col] = lsplit[n]        # if data is text
+                    line_data[col] = lsplit[n]  # if data is text
                 n += 1
 
             centroid_data.append(line_data)
             line = centroid_input.readline()
-    
+
     # Obtain unique packages
     refs = []
     packages = []
     for part in centroid_data:
-        packages.append(part['Package'])
-        #refs.append(part['Ref'][0])
-        comp = getref(part['Ref'])
+        packages.append(part["Package"])
+        # refs.append(part['Ref'][0])
+        comp = getref(part["Ref"])
         refs.append(comp)
 
     packages = set(packages)
     refs = set(refs)
 
-    # Generate colours programatically 
+    # Generate colours programatically
     colours = {}
     colour_list = plt.cm.tab20(np.linspace(0, 1, len(refs)))
     ccount = 0
@@ -173,41 +218,65 @@ def graph(centroid,libraries,top,bottom=''):
 
             for path in libraries:
                 path = os.path.expanduser(path)
-                data = load(path,package)
+                data = load(path, package)
                 if data != {}:
                     boarddata[package] = data
                     raise GetOutOfLoop
 
             for path in libraries:
-                for d in glob.glob(path+"/*/"):
-                    data = load(d,package)
+                for d in glob.glob(path + "/*/"):
+                    data = load(d, package)
                     if data != {}:
                         boarddata[package] = data
                         raise GetOutOfLoop
 
         except GetOutOfLoop:
-            outtahere=True
+            outtahere = True
 
-    image(refs,colours,centroid_data,boarddata,"top",top)
+    image(refs, colours, centroid_data, boarddata, "top", top)
 
-    if bottom != '':
-        image(refs,colours,centroid_data,boarddata,"bottom",bottom)
+    if bottom != "":
+        image(refs, colours, centroid_data, boarddata, "bottom", bottom)
+
 
 if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser(description="CLI for PCBA centroid parsing")
-    parser.add_argument("--input",dest="input", type=str, help="KiCad centroid file",required=True)
-    parser.add_argument("--libraries",dest="libraries", type=str, help="KiCad directories that have footprints, comma separated",required=True)
-    parser.add_argument("--top",dest="top",type=str, help="Image output for top",required=True)
-    parser.add_argument("--bottom",dest="bottom",type=str, help="Image output for bottom",required=False)
+    parser.add_argument(
+        "--input", dest="input", type=str, help="KiCad centroid file", required=True
+    )
+    parser.add_argument(
+        "--libraries",
+        dest="libraries",
+        type=str,
+        help="KiCad directories that have footprints, comma separated",
+        required=True,
+    )
+    parser.add_argument(
+        "--top", dest="top", type=str, help="Image output for top", required=True
+    )
+    parser.add_argument(
+        "--bottom",
+        dest="bottom",
+        type=str,
+        help="Image output for bottom",
+        required=False,
+    )
     args = parser.parse_args()
 
     # generate graph from centroid file
     if not args.bottom:
-        graph(os.path.expanduser(args.input),os.path.expanduser(args.top),args.libraries.split(","),top=os.path.expanduser(args.top))
+        graph(
+            os.path.expanduser(args.input),
+            os.path.expanduser(args.top),
+            args.libraries.split(","),
+            top=os.path.expanduser(args.top),
+        )
     else:
-        graph(os.path.expanduser(args.input),args.libraries.split(","),top=os.path.expanduser(args.top),bottom=os.path.expanduser(args.bottom))
-
-
-
+        graph(
+            os.path.expanduser(args.input),
+            args.libraries.split(","),
+            top=os.path.expanduser(args.top),
+            bottom=os.path.expanduser(args.bottom),
+        )
